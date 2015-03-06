5118c7be5b471300855f2cdb2c6265d021003ae6
Dicko Sow <s12.dsow@gmail.com>
2015-03-06 00:45:12 -0500

    updated code problem 2

--- problem_2.rb
+++ problem_2.rb
@@ -5,15 +5,6 @@
 
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-
-# def fib 
-# 	arr = [0, 1]
-# 	1.upto(32) do |x| 
-# 		arr[-1] + arr[-2] <= 4_000_000 ? arr << arr[-1] + arr[-2] : arr
-# 	end
-# 	arr.map { |i| i % 2 == 0 ? i : 0 }.reduce(:+)
-# end
-
 def fib 
 	arr = [0, 1]
 	1.upto(32) do |x| 
